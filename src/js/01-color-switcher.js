3fb59d10b174a45e08311cc5397459ebd315f551
Pavlo Bieliak <66524818+Paul48wt@users.noreply.github.com>
2022-09-25 21:33:04 +0300

    hw-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+let timerId = null;
+startBtn.addEventListener('click', onBtnStartClick);
+stopBtn.addEventListener('click', onBtnStopClick);
 
+function onBtnStartClick() {
+  timerId = setInterval(setBgColor, 1000);
+}
+
+function onBtnStopClick() {
+  clearInterval(timerId);
+}
+
+function setBgColor() {
+  body.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
