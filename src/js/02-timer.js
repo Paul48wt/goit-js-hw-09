504b02e1901bda2b959dd5603aedf3d7effbcdfc
Pavlo Bieliak <66524818+Paul48wt@users.noreply.github.com>
2022-09-27 21:32:33 +0300

    hw-09 prt02

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,33 +2,41 @@
 import flatpickr from 'flatpickr';
 // Дополнительный импорт стилей
 import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
 const startBtn = document.querySelector('button[data-start]');
 const daysValue = document.querySelector('span[data-days]');
 const hoursValue = document.querySelector('span[data-hours]');
 const minutesValue = document.querySelector('span[data-minutes]');
 const secondsValue = document.querySelector('span[data-seconds]');
-
+let startTime = 0;
+let timerId = null;
+const INTERVAL = 1000;
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
-  minuteIncrement: 10,
+  minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0].getTime());
+    // console.log(selectedDates[0].getTime());
     const date = new Date();
     if (date > selectedDates[0]) {
-      window.alert('Please choose a date in the future');
+      Notiflix.Notify.failure('Please choose a date in the future');
+      // window.alert('Please choose a date in the future');
     } else {
       startBtn.addEventListener('click', startCounter);
     }
-
-    updateClockFace(convertMs(selectedDates[0].getTime() - date.getTime()));
+    startTime = selectedDates[0].getTime() - date.getTime();
+    // updateClockFace(
+    //   convertMs(timerCounter(selectedDates[0].getTime() - date.getTime()))
+    // );
   },
 };
 
-const calendar = flatpickr('#datetime-picker', options);
+flatpickr('#datetime-picker', options);
 
-function startCounter() {}
+function startCounter() {
+  timerCounter(startTime);
+}
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
@@ -50,8 +58,23 @@ function convertMs(ms) {
 }
 
 function updateClockFace({ days, hours, minutes, seconds }) {
-  daysValue.textContent = `${days}`;
-  hoursValue.textContent = `${hours}`;
-  minutesValue.textContent = `${minutes}`;
-  secondsValue.textContent = `${seconds}`;
+  daysValue.textContent = addLeadingZero(days);
+  hoursValue.textContent = addLeadingZero(hours);
+  minutesValue.textContent = addLeadingZero(minutes);
+  secondsValue.textContent = addLeadingZero(seconds);
+}
+
+function timerCounter(ms) {
+  let timer = ms;
+  timerId = setInterval(() => {
+    timer = timer - INTERVAL;
+    updateClockFace(convertMs(timer));
+    if (timer <= 1000) {
+      clearInterval(timerId);
+    }
+  }, INTERVAL);
+}
+
+function addLeadingZero(value) {
+  return value.toString().padStart(2, '0');
 }
